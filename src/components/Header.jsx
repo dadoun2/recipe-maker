a890b4f8db33fc2edddb26111f59bbe26471b6f3
dadoun2 <dadoun801@gmail.com>
2025-05-13 06:30:52 +0100

    fifth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
     return (
     <header>
-        <img src="../images/chef.png" alt="Chef Logo" />
+        <img src="../../images/chef.png" alt="Chef Logo" />
         <h1>Recipe Maker </h1>
     </header>
 
