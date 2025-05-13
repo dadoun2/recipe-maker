e5902040b43732d6b2eed73e8c50addcb089fdcc
dadoun2 <dadoun801@gmail.com>
2025-05-13 13:49:20 +0100

    update recipe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import img from "/chef.png"
 export default function Header() {
     return (
     <header>
-        <img src={`${import.meta.env.BASE_URL}chef.png`} alt="Chef Logo" /> 
+        <img src={`${import.meta.env.BASE_URL}chef.png`} alt="Chef Logo" />
         <h1>Recipe Maker </h1>
     </header>
 
