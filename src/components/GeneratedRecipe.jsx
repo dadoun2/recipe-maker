4ca9d5f7290a9949094b915a9b5c9f9516bc9bc4
dadoun2 <dadoun801@gmail.com>
2025-05-13 11:08:37 +0100

    arranging images

--- src/components/GeneratedRecipe.jsx
+++ src/components/GeneratedRecipe.jsx
@@ -5,7 +5,7 @@ import ReactMarkdown from 'react-markdown'
 export default function GeneratedRecipe(props){
     return(
         <section>
-            <div aria-live="polite" style={{fontFamily:"Ink Free", marginTop:"50px",marginLeft:"30px", maxWidth:"450px"}}>
+            <div aria-live="polite" style={{fontFamily:"Ink Free", marginTop:"50px", maxWidth:"450px"}}>
                 <h1> Chef Suggests: </h1>
           <ReactMarkdown>{props.recipe}</ReactMarkdown>  
           </div>
