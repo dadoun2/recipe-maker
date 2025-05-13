ada7b2a0c62a5d6da5e65d50ce36852a65977e2d
dadoun2 <dadoun801@gmail.com>
2025-05-14 00:13:39 +0100

    update recipe

--- src/components/GeneratedRecipe.jsx
+++ src/components/GeneratedRecipe.jsx
@@ -3,12 +3,10 @@
 import ReactMarkdown from 'react-markdown'
 
 export default function GeneratedRecipe(props){
-
-
     return(
         <section>
             <div aria-live="polite" style={{fontFamily:"Patrick Hand, Ink Free, cursive",marginLeft:"10px", marginTop:"50px",marginRight:"10px", maxWidth:"450px"}}>
-                <h1> Chef Suggests: </h1>
+                <h1> Chef Recommends: </h1>
           <ReactMarkdown>{props.recipe}</ReactMarkdown>  
           </div>
         </section>
