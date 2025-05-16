5f2331ddcc5496d6c44e8f8723f4cc30a487eb5f
dadoun2 <dadoun801@gmail.com>
2025-05-16 16:44:33 +0100

    update

--- src/components/GeneratedRecipe.jsx
+++ src/components/GeneratedRecipe.jsx
@@ -1,16 +1,21 @@
+import ReactMarkdown from "react-markdown";
 
-
-import ReactMarkdown from 'react-markdown'
-
-export default function GeneratedRecipe(props){
-    return(
-        <section>
-            <div aria-live="polite" style={{fontFamily:"Patrick Hand, Ink Free, cursive",marginLeft:"10px", marginTop:"50px",marginRight:"10px", maxWidth:"450px"}}>
-                <h1> Chef Recommends: </h1>
-          <ReactMarkdown>{props.recipe}</ReactMarkdown>  
-          </div>
-        </section>
-
-
-    )
+export default function GeneratedRecipe(props) {
+  return (
+    <section>
+      <div
+        aria-live="polite"
+        style={{
+          fontFamily: "Patrick Hand, Ink Free, cursive",
+          marginLeft: "10px",
+          marginTop: "50px",
+          marginRight: "10px",
+          maxWidth: "450px",
+        }}
+      >
+        <h1> Chef Recommends: </h1>
+        <ReactMarkdown>{props.recipe}</ReactMarkdown>
+      </div>
+    </section>
+  );
 }
--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,55 +1,64 @@
+export default function IngredientsList(props) {
+  const ingredientList = props.ingredients.map((ingredient, index) => {
+    return (
+      <tr key={index}>
+        <td> - {ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}</td>
+        <td>
+          <button
+            className="btn-remove"
+            onClick={() => {
+              props.setIngredients((prevIngredients) => [
+                ...prevIngredients.filter((ingredient, i) => i !== index),
+              ]);
+            }}
+          >
+            Remove
+          </button>
+        </td>
+      </tr>
+    );
+  });
 
-
-export default function IngredientsList(props){
-
-const ingredientList=props.ingredients.map((ingredient, index) => {
-        return (
-
-            <tr key={index}> 
-            <td > - {ingredient.charAt(0).toUpperCase()+ingredient.slice(1)}</td> 
-             <td><button className="btn-remove" onClick={() => {
-                props.setIngredients(prevIngredients=>[...prevIngredients.filter((ingredient, i) => i !== index)])}
-            }>Remove</button>
-            </td>
-            </tr>     
-        )
-    })
-
-    return(
-        <section>
-             <div className="ingredients-list-header">
-                         {props.ingredients.length>0 ? <h2>Your list of ingredients🍴</h2> :  <h2>No ingredients added yet</h2>}
-                         {/*{ingredients.length>0 && <h2>Ingredients :</h2>}
+  return (
+    <section>
+      <div className="ingredients-list-header">
+        {props.ingredients.length > 0 ? (
+          <h2>Your list of ingredients🍴</h2>
+        ) : (
+          <h2>No ingredients added yet</h2>
+        )}
+        {/*{ingredients.length>0 && <h2>Ingredients :</h2>}
                          {ingredients.length===0 && <h2>No ingredients added yet</h2>}}
                          {/*conditional rendering forthe header based on the length of the ingredients array */}
-                    </div>
-                   
-         <table className="ingredients-list">
-                <tbody>
-                     {ingredientList}
-                </tbody>
-         </table>
-          { props.ingredients.length>1 ? 
-            <section>
-                <div className="recipe-ready">
-                    <div>
-                       <h4 style={{marginBottom:"0px"}}>Ready for a recipe ?</h4>
-                       <p style={{marginTop:"10px",fontSize:"15px"}}>Generate a recipe from your list of ingredients</p>
-                    </div>                   
-                        <button className="btn-recipe" onClick={props.getRecipe}>  Generate Recipe</button> 
-                    
-                </div> 
+      </div>
 
-            </section>: 
-            <section>
-                    <div className="recipe-ready">
-                    <div>
-                       <h4>Add more than 1 ingredient to Generate a recipe</h4>
-                       
-                    </div>
-                    </div>
-            </section> }
-            
-         </section>
-    )
+      <table className="ingredients-list">
+        <tbody>{ingredientList}</tbody>
+      </table>
+      {props.ingredients.length > 1 ? (
+        <section>
+          <div className="recipe-ready">
+            <div>
+              <h4 style={{ marginBottom: "0px" }}>Ready for a recipe ?</h4>
+              <p style={{ marginTop: "10px", fontSize: "15px" }}>
+                Generate a recipe from your list of ingredients
+              </p>
+            </div>
+            <button className="btn-recipe" onClick={props.getRecipe}>
+              {" "}
+              Generate Recipe
+            </button>
+          </div>
+        </section>
+      ) : (
+        <section>
+          <div className="recipe-ready">
+            <div>
+              <h4>Add more than 1 ingredient to Generate a recipe</h4>
+            </div>
+          </div>
+        </section>
+      )}
+    </section>
+  );
 }
